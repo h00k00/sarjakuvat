0ae8432bfb9b2f28b8a2c19ce409440ce5f5ff36
Harri Kaasalainen <harri.kaasalainen@gmail.com>
2016-06-30 22:43:59 +0300

    sarjat json konversiot

--- sarjat/sarjatconvert.py
+++ sarjat/sarjatconvert.py
@@ -11,7 +11,7 @@ lines = [line.strip() for line in open(sys.argv[1])]
 resultFile = open(sys.argv[2],'a')
 
 
-lines[:] = [line for line in lines if line.find("a class")>=0 or line.find("<h1>") >= 0 or line.find("</body>") >= 0]
+lines[:] = [line for line in lines if line.find("a class")>=0 or line.find("<h1>") >= 0 or line.find("</body>") >= 0 or line.find("<br />") >= 0]
 
 for line in lines:
 	line = line.replace("> ", ">")
@@ -29,6 +29,9 @@ for line in lines:
 	line = line.replace("</a>", "")
 	line = line.replace("</li>", "")
 	line = line.replace("<br/>", "")
+	line = line.replace("<br />", "")
+	line = line.replace("(", "")
+	line = line.replace(")", "")
 	line = line.replace("</html>", "")
 	line = line.replace("</body>", "]\n}\n]\n}")
 	print (line)
