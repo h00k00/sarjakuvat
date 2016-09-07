c7e85c499bbfc89f531b85cfa5e40df839971948
Harri Kaasalainen <harri.kaasalainen@gmail.com>
2016-09-07 12:04:28 +0300

    routing - first version

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,52 @@
-var app = angular.module('comics', ['ngMaterial']);
+var app = angular.module('comics', ['ngMaterial', 'ui.router']);
 
-app.controller('MainController', function ($scope, $http) {
-    $scope.tarinalista = [];
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/tab/dash');
+    $stateProvider
+    .state('albumit', {
+        url: "/albumit",
+        templateUrl: "partials/view1.html"
+    })
+    .state('lehdet', {
+        url: "/lehdet",
+        templateUrl: "partials/view2.html"
+    })
+    .state('sarjat', {
+        url: "/sarjat",
+        templateUrl: "partials/view3.html"
+    })
+    .state('tekijat', {
+        url: "/tekijat",
+        templateUrl: "partials/view4.html"
+    })
+    ;
+});
+
+app.controller('tabCtrl', function($scope, $location, $log, $http) {
+    $scope.selectedIndex = 0;
     $scope.showMenu = true;
     $scope.showList = false;
 
-    this.getMagazine = function (filename) {
+    $scope.$watch('selectedIndex', function(current, old) {
+      switch (current) {
+        case 0:
+          $location.url("/albumit");
+          break;
+        case 1:
+          $location.url("/lehdet");
+          break;
+        case 2:
+          $location.url("/sarjat");
+          break;
+        case 3:
+          $location.url("/tekijat");
+          break;
+      }
+    });
+
+    $scope.getContent = function (filename) {
         $http.get(filename).success(function (data) {
-            console.log(data);
             $scope.lehdennimi = data.lehdennimi;
             $scope.tarinalista = data.tarinalista;
             $scope.showMenu = false;
@@ -15,12 +54,10 @@ app.controller('MainController', function ($scope, $http) {
         }).error(function (error) {
             console.log("Error");
         });
-        console.log($scope.tarinalista);
     };
-    
-    this.showMenu = function () {
+
+    $scope.showMenulist = function () {
         $scope.showMenu = true;
         $scope.showList = false;
     };
-
 });
